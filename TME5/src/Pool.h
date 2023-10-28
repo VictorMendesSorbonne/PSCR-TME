99fff900248a71f3604bf9b8dd682dff4d0c331f
Haleront <Victor.Mendes_Vintena@etu.sorbonne-universite.fr>
2023-10-28 14:49:45 +0200

    Avancement TP5

--- TME5/src/Pool.h
+++ TME5/src/Pool.h
@@ -12,10 +12,34 @@ class Pool {
 	std::vector<std::thread> threads;
 public:
 	Pool(int qsize) ;
-	void start (int nbthread);
-	void submit (Job * job) ;
-	void stop() ;
-	~Pool() ;
+	void start (int nbthread){
+		threads.reserve(nbthread);
+		for(int i=0;i<nbthread;i++){
+			threads.emplace_back(poolWorker,&queue);
+		}
+	}
+
+	void submit (Job * job) {
+		queue.push(job);
+	}
+	void stop() {
+		queue.setBlocking(false);
+		for (auto &t:threads){
+			t.join();
+		}
+		threads.clear();
+	}
+
+	void poolWorker(Queue<Job> &queue){
+		while(true){
+			Job *pjob = queue.pop();
+			pjob->run();
+			delete pjob;
+		}
+	}
+	~Pool(){
+		stop();
+	}
 };
 
 }
--- TME5/src/Queue.h
+++ TME5/src/Queue.h
@@ -3,7 +3,7 @@
 
 #include <cstdlib>
 #include <mutex>
-
+#include <condition_variable>
 namespace pr {
 
 // MT safe version of the Queue, non blocking.
@@ -13,7 +13,9 @@ class Queue {
 	const size_t allocsize;
 	size_t begin;
 	size_t sz;
+	bool isBlocking;
 	mutable std::mutex m;
+	std::condition_variable cv;
 
 	// fonctions private, sans protection mutex
 	bool empty() const {
@@ -23,7 +25,7 @@ class Queue {
 		return sz == allocsize;
 	}
 public:
-	Queue(size_t size) :allocsize(size), begin(0), sz(0) {
+	Queue(size_t size) :allocsize(size), begin(0), sz(0), isBlocking(false){
 		tab = new T*[size];
 		memset(tab, 0, size * sizeof(T*));
 	}
@@ -33,8 +35,14 @@ public:
 	}
 	T* pop() {
 		std::unique_lock<std::mutex> lg(m);
+		while(empty() && isBlocking){
+			cv.wait(lg);
+		}
 		if (empty()) {
 			return nullptr;
+		}
+		if(full()){
+			cv.notify_all();
 		}
 		auto ret = tab[begin];
 		tab[begin] = nullptr;
@@ -44,12 +52,23 @@ public:
 	}
 	bool push(T* elt) {
 		std::unique_lock<std::mutex> lg(m);
+		while(full() && isBlocking){
+			cv.wait(lg);
+		}
 		if (full()) {
 			return false;
+		}
+		if(empty()){
+			cv.notify_all();
 		}
 		tab[(begin + sz) % allocsize] = elt;
 		sz++;
 		return true;
+	}
+	void setBlocking(bool isBlocking){
+		std::unique_lock<std::mutex> lg(m);
+		this->isBlocking =isBlocking;
+		cv.notify_all();
 	}
 	~Queue() {
 		// ?? lock a priori inutile, ne pas detruire si on travaille encore avec
